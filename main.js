bc00b5c6e80ccf082a46edf6f7166122df152230
FedotovFrontalex <permmedium@yandex.ru>
2024-02-21 12:33:36 +0500

    mend

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 import {geoService} from "./src/services/geoService/geoService.js";
 import {mapService} from "./src/services/mapService/mapService.js";
 
